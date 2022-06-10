e19f622a494894b9ea05870a85eeb019aad2c4d2
thangabcd <106944369+thangabcd@users.noreply.github.com>
2022-06-10 10:53:59 +0700

    Update ListMV.cs

--- ListMV.cs
+++ ListMV.cs
@@ -40,20 +40,20 @@ namespace TungMovie
 			if (comboBox1.Text == "find by id")
 			{
 				int id = Convert.ToInt32(txtSearch.Text.ToString());
-				SqlConnection conn = new SqlConnection(@"Data Source=PC;Initial Catalog=Movie_ticket_management;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"); //chuỗi kết nối
-				SqlCommand cmd = new SqlCommand("SELECT  [dbo].[fu_Find_all_movies_by_id](@movieid id) ", conn);
-				//cmd.Parameters.AddWithValue("@movieid", id);
+				SqlConnection conn = new SqlConnection(@"Data Source=MSI;Initial Catalog=Movie_ticket_management;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"); //chuỗi kết nối
+				SqlCommand cmd = new SqlCommand("SELECT movieid 'ID',name 'Movie',thumbnail 'Image', rating 'Rating', genre 'Genre', short_description 'Desciption', duration 'Duration', trailer 'Trailer' FROM Movie WHERE movieid LIKE '%" + id + "%' ", conn);
+				cmd.Parameters.AddWithValue("@movieid", id);
 				SqlDataAdapter da = new SqlDataAdapter(cmd);
 				DataTable dt = new DataTable();
 				da.Fill(dt);
 				DataTable x = dt;
-				gridListMV.DataSource = x ;
+				gridListMV.DataSource = x;
 			}
 			else if (comboBox1.Text == "find by name")
 			{
 				string name = txtSearch.Text.ToString();
-				SqlConnection conn = new SqlConnection(@"Data Source=PC;Initial Catalog=Movie_ticket_management;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"); //chuỗi kết nối
-				SqlCommand cmd = new SqlCommand("SELECT  [dbo].[fu_Find_all_movies_by_name](@name VARCHAR(MAX) ", conn);
+				SqlConnection conn = new SqlConnection(@"Data Source=MSI;Initial Catalog=Movie_ticket_management;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"); //chuỗi kết nối
+				SqlCommand cmd = new SqlCommand("SELECT movieid 'ID',name 'Movie',thumbnail 'Image', rating 'Rating', genre 'Genre', short_description 'Desciption', duration 'Duration', trailer 'Trailer' FROM Movie WHERE name LIKE '%" + name + "%' ", conn);
 				cmd.Parameters.AddWithValue("@name", name);
 				SqlDataAdapter da = new SqlDataAdapter(cmd);
 				DataTable dt = new DataTable();
@@ -65,7 +65,35 @@ namespace TungMovie
 
         private void txtSearch_TextChanged(object sender, EventArgs e)
         {
-
+		if (comboBox1.Text == "find by id")
+		{
+			if(txtSearch.Text == string.Empty)
+                {
+			ListMV_grid_Load();
+			return;
+                }
+			int id = Convert.ToInt32(txtSearch.Text.ToString());
+			SqlConnection conn = new SqlConnection(@"Data Source=MSI;Initial Catalog=Movie_ticket_management;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"); //chuỗi kết nối
+			SqlCommand cmd = new SqlCommand("SELECT movieid 'ID',name 'Movie',thumbnail 'Image', rating 'Rating', genre 'Genre', short_description 'Desciption', duration 'Duration', trailer 'Trailer' FROM Movie WHERE movieid LIKE '%" + id + "%' ", conn);
+			cmd.Parameters.AddWithValue("@movieid", id);
+			SqlDataAdapter da = new SqlDataAdapter(cmd);
+			DataTable dt = new DataTable();
+			da.Fill(dt);
+			DataTable x = dt;
+			gridListMV.DataSource = x;
+		}
+		else if (comboBox1.Text == "find by name")
+		{
+			string name = txtSearch.Text.ToString();
+			SqlConnection conn = new SqlConnection(@"Data Source=MSI;Initial Catalog=Movie_ticket_management;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"); //chuỗi kết nối
+			SqlCommand cmd = new SqlCommand("SELECT movieid 'ID',name 'Movie',thumbnail 'Image', rating 'Rating', genre 'Genre', short_description 'Desciption', duration 'Duration', trailer 'Trailer' FROM Movie WHERE name LIKE '%" + name + "%' ", conn);
+			cmd.Parameters.AddWithValue("@name", name);
+			SqlDataAdapter da = new SqlDataAdapter(cmd);
+			DataTable dt = new DataTable();
+			da.Fill(dt);
+			DataTable x = dt;
+			gridListMV.DataSource = x;
+		}
         }
     }
 }
